641cb3c82c0fb9c2d5bf01758ed99154e6ca564e
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-02-24 13:00:41 +0300

    Update global CSS and font imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ["latin"] });
+const fkGrotesk = localFont({
+  src: '../public/fonts/fk-grotesk-neue-regular.otf',
+  display: 'swap',
+})
+
+
+
 
 export const metadata: Metadata = {
   title: "Read This Book Next",
@@ -16,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={fkGrotesk.className}>{children}</body>
     </html>
   );
 }
