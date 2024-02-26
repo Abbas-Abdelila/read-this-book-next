d30e2431e60b071c70d649963ba457498f05c2dd
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-02-26 15:47:59 +0300

    Refactor book search functionality and add book selection

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useState, useEffect } from "react";
 import { CheckboxReactHookFormMultiple } from "@/components/SelectGenre";
@@ -8,43 +8,47 @@ import { PlusIcon } from "lucide-react";
 import Image from "next/image";
 import axios from "axios";
 import { useDebounce } from "use-debounce";
-
+import BookImage from "@/components/BookImage";
+import { BookSearchResult } from "@/types/types";
+import { Book } from "@/types/types";
 
 export default function Home() {
-
-
-  const [searchResults, setSearchResults] = useState([]);
-  const [text, setText] = useState('');
+  const [searchResults, setSearchResults] = useState<BookSearchResult[]>([]);
+  const [text, setText] = useState("");
+  const [displaySearchResults, setDisplaySearchResults] = useState(false);
+  const [selectedBooks, setSelectedBooks] = useState<BookSearchResult[]>([]);
   // Debounce the input change
   const [query] = useDebounce(text, 500);
 
   useEffect(() => {
-    
     const fetchBooks = async () => {
-      if (query === '') {
-        setSearchResults([]);
+      if (query === "") {
+        setDisplaySearchResults(false);
         return;
       }
-      
+
       try {
-        const { data } = await axios.get(`https://openlibrary.org/search.json?title=${query}&page=1&limit=10`);
+        const { data }: { data: Book } = await axios.get(
+          `https://openlibrary.org/search.json?title=${query}&page=1&limit=10`
+        );
         setSearchResults(data.docs);
         console.log(searchResults);
-      }
-      catch (error) {
+      } catch (error) {
         console.error(error);
       }
-
-    }
+    };
     fetchBooks();
-      
-    
   }, [query]);
 
-
-
-
-
+  const handleBookSelection = (event: React.MouseEvent<HTMLLIElement>, book: BookSearchResult) => {
+    event.preventDefault(); // Prevent the default action of the click event
+    // Your logic to handle book selection
+    setSelectedBooks([...selectedBooks, book]);
+    console.log(selectedBooks)
+  };
+  const handleBookRemoval = (index: number) => {
+    setSelectedBooks(selectedBooks.filter((_, i) => i !== index));
+  };
 
   return (
     <main className="w-[90%] mx-auto my-10 py-10 bg-white rounded-xl">
@@ -59,6 +63,7 @@ export default function Home() {
             height={1024}
             alt="Read This Next"
             className="w-48 h-48"
+            priority={true}
           />
         </div>
 
@@ -67,41 +72,65 @@ export default function Home() {
         </h3>
         <p className="text-gray-500">(add at least 3)</p>
 
-        <div className={`w-[80%] mt-10 px-6 py-3 border border-gray-400 rounded-3xl ${searchResults.length > 0 ? 'divide-y divide-gray-300' : ''} `}>
-          <div className={`flex space-x-3 items-center ${searchResults.length > 0 && 'pb-3'}`}>
+        <div className="flex space-x-3 mt-10">
+          {selectedBooks.map((book, index) => (
+            <BookImage
+              key={index}
+              url={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`} // Assuming the book object has a url property
+              title={book.title} // Assuming the book object has a title property
+              onRemove={() => handleBookRemoval(index)}
+            />
+          ))}
+        </div>
+        <div
+          className={`w-[80%] lg:max-w-2xl  mt-10 px-6 py-3 border border-gray-400 rounded-3xl ${
+            searchResults.length > 0 && displaySearchResults
+              ? "divide-y divide-gray-300"
+              : ""
+          } search-results-container`}
+        >
+          <div
+            className={`flex space-x-3 items-center ${
+              searchResults.length > 0 && displaySearchResults && "pb-3"
+            }`}
+          >
             <SearchIcon className="w-6 h-6 text-blue-600 cursor-pointer" />
             <input
               type="text"
-              placeholder="Search for a book or author"
+              placeholder="search books to add"
               onChange={(e) => setText(e.target.value)}
               className="focus:outline-none focus:shadow-none w-full font-extralight text-gray-500"
-              onBlur={() => setSearchResults([])}
-              onFocus={() => setSearchResults([])}
+              onBlur={(e) => {
+                // Check if the related target is within the search results container
+                if (!e.relatedTarget || !e.relatedTarget.closest('.search-results-container')) {
+                  // Delay hiding the search results by  200 milliseconds
+                  setTimeout(() => {
+                    setDisplaySearchResults(false);
+                  },  200);
+                }
+              }}
+              onFocus={() => setDisplaySearchResults(true)}
             />
           </div>
-          <div className={`text-left ${searchResults.length > 0 ? 'pt-3' : 'h-0'}`}>
+          <div
+            className={`text-left ${
+              searchResults.length > 0 && displaySearchResults ? "pt-3" : "h-0"
+            }`}
+          >
             <ul>
-              { searchResults?.slice(0,5).map((result, index) => 
-              (<li key={index} className="text-md text-gray-500 px-1 py-2 flex justify-between hover:bg-slate-200 bg-opacity-50 rounded-md">
-              {result.title}
-              <PlusIcon className="text-blue-600 cursor-pointer" />
-            </li>)
-              )}
-              {/* <li className="text-md text-gray-500 px-1 py-2 flex justify-between hover:bg-slate-200 bg-opacity-50 rounded-md">
-                Example: Harry Potter, J.K. Rowling
-                <PlusIcon className="text-blue-600 cursor-pointer" />
-              </li>
-              <li className="text-md text-gray-500 px-1 py-2 flex justify-between hover:bg-slate-200 bg-opacity-50 rounded-md">
-                Example: Harry Potter, J.K. Rowling
-                <PlusIcon className="text-blue-600 cursor-pointer" />
-              </li>
-              <li className="text-md text-gray-500 px-1 py-2 flex justify-between hover:bg-slate-200 bg-opacity-50 rounded-md">
-                Example: Harry Potter, J.K. Rowling
-                <PlusIcon className="text-blue-600 hover:text-blue-800 cursor-pointer" />
-              </li>
-              <li className="text-md text-gray-500 px-1 py-2 hover:bg-slate-200 bg-opacity-50 rounded-md">
-                Example: The Hobbit, J.R.R. Tolkien
-              </li> */}
+              {displaySearchResults &&
+                searchResults?.slice(0, 5).map((result, index) => (
+                  <li
+                    key={index}
+                    onClick={(event) => handleBookSelection(event, result)}
+                    className="text-md text-gray-500 px-1 py-2 flex justify-between hover:bg-slate-200 bg-opacity-50 rounded-md"
+                  >
+                    {result.title}
+                    <PlusIcon
+                      className="text-blue-600 cursor-pointer"
+                    />
+                  </li>
+                ))}
             </ul>
           </div>
         </div>
@@ -114,9 +143,7 @@ export default function Home() {
         </div>
       </div>
 
-        <Suggestion />
-
+      <Suggestion />
     </main>
   );
 }
-
