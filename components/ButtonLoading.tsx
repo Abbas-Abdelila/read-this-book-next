b9ffd7dd8e280fe3dc945d4c60e3446e3005b1fb
Abbas-Abdelila <abbasabdelila@gmail.com>
2024-03-03 17:47:17 +0300

    Update button text to "Generating..."

--- components/ButtonLoading.tsx
+++ components/ButtonLoading.tsx
@@ -6,7 +6,7 @@ export function ButtonLoading() {
   return (
     <Button size="sm" className="bg-blue-600 hover:bg-blue-500 transition-transform duration-150 ease-in" disabled>
       <ReloadIcon className="mr-2 h-4 w-4 md:h-5 md:w-5 animate-spin" />
-      Please wait
+      Generating...
     </Button>
   )
 }
